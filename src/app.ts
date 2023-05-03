05ba911ad659eb43c11fea5f0a3acd257d91f250
Maksyusha <pirate227@yandex.ru>
2023-05-03 21:50:30 +0300

    Исправил ошибки, выдаваемые линтером

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,29 @@
-import express, { NextFunction, Request, Response } from "express";
-import mongoose from "mongoose";
-import routes from './routes/index'
-import handleErrors from "./middlewares/handleErrors";
-import NotFoundError from "./types/errors/classes/not-found-error";
+import express, { NextFunction, Request, Response } from 'express';
+import mongoose from 'mongoose';
+import routes from './routes/index';
+import handleErrors from './middlewares/handleErrors';
+import NotFoundError from './types/errors/classes/not-found-error';
+import { NOT_FOUND_MESSAGE } from './types/errors/error-messages';
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect("mongodb://0.0.0.0:27017/mestodb");
+mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   (req as any).user = {
-    _id: '6447b54e3153a828191dae01'
+    _id: '6447b54e3153a828191dae01',
   };
 
   next();
 });
-app.use('/', routes)
+app.use('/', routes);
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
-  next(new NotFoundError('404'))
-})
-app.use(handleErrors)
+  next(new NotFoundError(NOT_FOUND_MESSAGE));
+});
+app.use(handleErrors);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
--- src/types/errors/classes/not-found-error.ts
+++ src/types/errors/classes/not-found-error.ts
@@ -1,10 +1,10 @@
-import { constants } from "http2"
+import { constants } from 'http2';
 
 export default class NotFoundError extends Error {
-  public statusCode: number
+  public statusCode: number;
 
   constructor(message: string) {
-    super(message)
-    this.statusCode = constants.HTTP_STATUS_NOT_FOUND
+    super(message);
+    this.statusCode = constants.HTTP_STATUS_NOT_FOUND;
   }
 }
